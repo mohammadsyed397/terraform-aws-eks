5d61d357390e47a6c2081c87a268a5ec8faa4ed9
mohammadsyed397 <mohammad.syed601@gmail.com>
2025-10-05 12:52:38 -0400

    latest

---
+++ 60-acm/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+
+    source = "hashicorp/aws"
+    version = "5.98.0"
+    }
+  }
+  backend "s3" {
+    bucket        = "terraform-roboshop-prac"
+    key           = "roboshop-infra-dev-ACM"
+    region        = "us-east-1"
+    encrypt       = true
+
+  }
+}
+provider "aws" {
+    region = "us-east-1"
+  
+}
---
+++ 60-acm/variables.tf
@@ -0,0 +1,12 @@
+variable "zone_name" {
+    default = "robosyed.space"
+}
+variable "project" {
+    default = "roboshop"
+}
+variable "environment" {
+    default = "dev"
+}
+variable "zone_id" {
+    default = "Z01504851BCZC2Y77VRX8"
+}
