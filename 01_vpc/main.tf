ec0e80eb7a115ccd7558d8272dbb67a59b5413dd
mohammadsyed397 <mohammad.syed601@gmail.com>
2025-10-14 21:12:43 -0400

    latest

--- 01_vpc/main.tf
+++ 01_vpc/main.tf
@@ -5,6 +5,7 @@ module "vpc" {
     public_cidr_blocks = var.public_subnet_cidrs
     private_cidr_blocks = var.private_subnet_cidrs
     database_subnet_blocks = var.database_subnet_cidrs
+    
     is_peering_required = true
     
     
